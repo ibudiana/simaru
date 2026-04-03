8e7fbbd8d0bfc165e8c942a0fa5acc41b0933d4b
I Wayan Budiana <ibudiana@student.ciputra.ac.id>
2026-04-03 15:02:38 +0800

    update package

--- app/generated/prisma/internal/class.ts
+++ app/generated/prisma/internal/class.ts
@@ -23,7 +23,7 @@ const config: runtime.GetPrismaClientConfig = {
       "value": "prisma-client"
     },
     "output": {
-      "value": "/Users/ian/project/learning/webpage/simaru-next-js/app/generated/prisma",
+      "value": "/Users/ian/project/learning/webpage/simaru/app/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -37,7 +37,7 @@ const config: runtime.GetPrismaClientConfig = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/Users/ian/project/learning/webpage/simaru-next-js/prisma/schema.prisma",
+    "sourceFilePath": "/Users/ian/project/learning/webpage/simaru/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativePath": "../../../prisma",
